ca06d0e0f32b4a4eb4ddbc7413cbc754d88d3683
bouge2 <nicolas.bouge@etu.unilim.fr>
2023-10-04 16:53:21 +0200

    commit

---
+++ src/Controller/Admin/UsersCrudController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Users;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ArrayField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+
+class UsersCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return Users::class;
+    }
+
+
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            TextField::new('email'),
+            TextField::new('password'),
+            ArrayField::new('roles'),
+        ];
+    }
+
+}
